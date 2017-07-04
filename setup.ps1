550ef5f368bb35835c7bbdae2d50cc91dc608618
Carlos Barajas <soldotsol@gmail.com>
2017-07-04 12:22:10 -0700

    Download DNN package as a seperate image layer

--- setup.ps1
+++ setup.ps1
@@ -3,21 +3,14 @@ Import-Module IISAdministration
 #==================
 # Global Flags ====
 $ErrorActionPreference = 'Stop'
-$ProgressPreference = 'SilentlyContinue'
 #==================
 # Variables =======
-$version = "9.1.0"
-$buildCounter = "367"
-$fullVersion = "$version.$buildcounter"
-$packageUrl = "https://github.com/dnnsoftware/Dnn.Platform/releases/download/v${version}/DNN_Platform_${fullversion}_Install.zip"
 $sitesDir = "C:/Sites"
 $websiteDir = "$sitesDir/DotNetNuke"
 $zipFile = "C:/DotNetNuke.zip"
 #==================
 # Main ============
-New-Item -ItemType directory -Path $sitesDir -Force
-
-Invoke-WebRequest $packageUrl -OutFile $zipFile -UseBasicParsing
+New-Item -ItemType directory -Path $sitesDir -Force | Out-Null
 
 Expand-Archive -LiteralPath $zipFile -DestinationPath $websiteDir
 New-IISSite -Name "DotNetNuke" `
