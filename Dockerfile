ab8dd6fbbf90d4d1483441fe5afbb5a3f7eae212
Carlos Barajas <soldotsol@gmail.com>
2017-07-04 07:49:57 -0700

    Remove zip package in docker container, make setup script download from github.

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,6 @@
 FROM microsoft/iis
 MAINTAINER github.com/solthoth
 
-ADD DNN_Platform_9.1.0.367_Install.zip /DotNetNuke.zip
 ADD setup.ps1 /setup.ps1
 
 RUN powershell -NoProfile -File  C:\setup.ps1
--- setup.ps1
+++ setup.ps1
@@ -1,14 +1,25 @@
 # Imports =========
 Import-Module IISAdministration
 #==================
-
-# Variables =======
-$websiteDir = "C:/Sites/DotNetNuke"
+# Global Flags ====
+$ErrorActionPreference = 'Stop'
+$ProgressPreference = 'SilentlyContinue'
 #==================
-
+# Variables =======
+$version = "9.1.0"
+$buildCounter = "367"
+$fullVersion = "$version.$buildcounter"
+$packageUrl = "https://github.com/dnnsoftware/Dnn.Platform/releases/download/v${version}/DNN_Platform_${fullversion}_Install.zip"
+$sitesDir = "C:/Sites"
+$websiteDir = "$sitesDir/DotNetNuke"
+$zipFile = "C:/DotNetNuke.zip"
+#==================
 # Main ============
-mkdir C:/Sites
-Expand-Archive -LiteralPath "C:/DotNetNuke.zip" -DestinationPath $websiteDir
+New-Item -ItemType directory -Path $sitesDir -Force
+
+Invoke-WebRequest $packageUrl -OutFile $zipFile -UseBasicParsing
+
+Expand-Archive -LiteralPath $zipFile -DestinationPath $websiteDir
 New-IISSite -Name "DotNetNuke" `
             -PhysicalPath $websiteDir `
             -BindingInformation "*:3000:"
